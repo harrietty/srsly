2186fe7531b4d75a74bbe5816a75fd5cdc234cab
Harriet Ryder <harriethryder@gmail.com>
2019-01-15 08:10:29 +0000

    Add Node server

---
+++ app.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
